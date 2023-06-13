9de36b28ff657773021f47a4a5a970826fab8d14
Pratik Gawand <gawand.p@northeastern.edu>
2023-06-13 09:51:56 -0400

    Proper setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'dell',
+      database: 'nestjs-admin',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
